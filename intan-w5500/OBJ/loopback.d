4c7807bcb8b386705d8f31ab3c2474add153ed00
lyndon <liuzhaoxuchn@163.com>
2017-08-01 20:49:03 +0800

    sysclock=168M,spi_div / 2

---
+++ intan-w5500/OBJ/loopback.d
@@ -0,0 +1,9 @@
+..\obj\loopback.o: ..\Application\loopback.c
+..\obj\loopback.o: ..\Application\loopback.h
+..\obj\loopback.o: f:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+..\obj\loopback.o: ..\Ethernet\W5500/w5500.h
+..\obj\loopback.o: ..\USER\common.h
+..\obj\loopback.o: ..\Ethernet\W5500/../wizchip_conf.h
+..\obj\loopback.o: ..\Ethernet\W5500/../W5500/w5500.h
+..\obj\loopback.o: ..\Ethernet\socket.h
+..\obj\loopback.o: f:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
---
+++ intan-w5500/OBJ/wizchip_conf.d
@@ -0,0 +1,7 @@
+..\obj\wizchip_conf.o: ..\Ethernet\wizchip_conf.c
+..\obj\wizchip_conf.o: f:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+..\obj\wizchip_conf.o: ..\Ethernet\wizchip_conf.h
+..\obj\wizchip_conf.o: f:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+..\obj\wizchip_conf.o: ..\Ethernet\W5500/w5500.h
+..\obj\wizchip_conf.o: ..\USER\common.h
+..\obj\wizchip_conf.o: ..\Ethernet\W5500/../wizchip_conf.h
